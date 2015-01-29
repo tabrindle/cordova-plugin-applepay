5c3df10460e85d635145c444e28ba25060ad3b6f
Trevor Brindle <tabrindle@gmail.com>
2015-01-29 12:35:03 -0500

    Rewrite JS

--- www/applepay.js
+++ www/applepay.js
@@ -1,18 +1,32 @@
+var ApplePay = function() {};
 
-var argscheck = require('cordova/argscheck'),
-    utils = require('cordova/utils'),
-    exec = require('cordova/exec');
-    
-var ApplePay = {
-    
-    setMerchantId: function(successCallback, errorCallback, merchantId) {
-        exec(successCallback, errorCallback, "ApplePay", "setMerchantId", [merchantId]);
-    },
-    
-    makePaymentRequest: function(successCallback, errorCallback, order) {
-        exec(successCallback, errorCallback, "ApplePay", "makePaymentRequest", [order]);
-    }
-    
+ApplePay.prototype.GenericSuccessCallback = function(response) {
+    console.log('Success');
+    console.log(response);
 };
 
-module.exports = ApplePay;
+ApplePay.prototype.GenericFailureCallback = function(response) {
+    console.log('Failure');
+    console.log(response);
+};
+
+ApplePay.prototype.setMerchantId: function(merchantId, SuccessCallback, FailureCallback) {
+    console.log('setMerchantId');
+    if (SuccessCallback && FailureCallback) {
+        cordova.exec(SuccessCallback, FailureCallback, 'ApplePay', 'setMerchantId', [merchantId]);
+    } else {
+        cordova.exec(ApplePay.GenericSuccessCallback, ApplePay.GenericFailureCallback, 'ApplePay', 'setMerchantId', [merchantId]);
+    }
+};
+
+ApplePay.prototype.makePaymentRequest: function(order, SuccessCallback, FailureCallback) {
+    console.log('makePaymentRequest');
+    if (SuccessCallback && FailureCallback) {
+        cordova.exec(SuccessCallback, FailureCallback, 'ApplePay', 'makePaymentRequest', [order]);
+    } else {
+        cordova.exec(ApplePay.GenericSuccessCallback, ApplePay.GenericFailureCallback, 'ApplePay', 'makePaymentRequest', [order]);
+    }
+};
+
+var ApplePay = new AdTheorentManager();
+module.exports = ApplePayApplePay;
